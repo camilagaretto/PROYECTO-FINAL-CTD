a9cc87fa195cd8bb6fc97a63fda62496e45a0e69
Alanis Gomez <agomez@aper.com>
2023-08-08 00:41:40 -0300

    Fix ruta de imagen

--- frontend/src/Routes/Admin/AddTeacherPage/AddTeacherPage.jsx
+++ frontend/src/Routes/Admin/AddTeacherPage/AddTeacherPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import TeacherForm from '../../../Components/TeacherForm/TeacherForm'
 import './AddTeacherPage.scss'
-import Banner from "../../..//teacher-form.svg";
+import Banner from "../../../assets/teacher-form.svg";
 
 const AddTeacherPage = () => {
 
