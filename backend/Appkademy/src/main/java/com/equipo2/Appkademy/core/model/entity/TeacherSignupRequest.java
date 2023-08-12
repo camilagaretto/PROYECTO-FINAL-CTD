5286aec9a3e2591b01fd1f15d7d58a4e49f27338
julianbordet <bordet.julian@gmail.com>
2023-08-12 10:38:18 -0300

    added jacoco setup and create and terminate request model

---
+++ backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/model/entity/TeacherSignupRequest.java
@@ -0,0 +1,31 @@
+package com.equipo2.Appkademy.core.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "teacher_signup_request")
+public class TeacherSignupRequest extends BaseSqlEntity<Long> {
+
+    @Column(name = "request_created_on")
+    private LocalDateTime requestCreatedOn;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", unique = true)
+    private Teacher teacher;
+
+    //deberían ser atributos del teacher createRequestDto
+    //private MultipartFile profilePicture;
+    //private List<MultipartFile> secondaryPictures;
+    //private List<MultipartFile> identityValidationDocuents;
+
+}
