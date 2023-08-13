43cdbffc1cc4d6610c77ee1764f35e69e0b8c312
julianbordet <bordet.julian@gmail.com>
2023-08-12 22:18:45 -0300

    added validation to check if new appointment falls within teacher working day

--- backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/service/impl/ScheduledAppointmentServiceImpl.java
+++ backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/service/impl/ScheduledAppointmentServiceImpl.java
@@ -3,6 +3,7 @@ package com.equipo2.Appkademy.core.service.impl;
 import com.equipo2.Appkademy.core.mapper.AppkademyMapper;
 import com.equipo2.Appkademy.core.model.entity.ScheduledAppointment;
 import com.equipo2.Appkademy.core.model.entity.Teacher;
+import com.equipo2.Appkademy.core.model.entity.WeeklyWorkingSchedule;
 import com.equipo2.Appkademy.core.model.repository.ScheduledAppointmentRepository;
 import com.equipo2.Appkademy.core.model.repository.StudentRepository;
 import com.equipo2.Appkademy.core.model.repository.TeacherRepository;
@@ -15,7 +16,9 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -39,7 +42,8 @@ public class ScheduledAppointmentServiceImpl implements ScheduledAppointmentServ
             assertStudentExists(createRequestDto.getStudentId());
             assertStartAndEndDateAreInTheFuture(createRequestDto);
             assertStartDateIsBeforeEndDate(createRequestDto);
-            assertTeacherScheduleIsAvailable(teacher, createRequestDto);
+            assertAppointmentFallsWithinTeacherWorkingSchedule(teacher, createRequestDto);
+            assertTeacherTimeSlotIsAvailable(teacher, createRequestDto);
 
             ScheduledAppointment newAppointment = new ScheduledAppointment();
             newAppointment.setStartsOn(createRequestDto.getStartsOn());
@@ -50,6 +54,47 @@ public class ScheduledAppointmentServiceImpl implements ScheduledAppointmentServ
             return scheduledAppointmentRepository.save(newAppointment);
     }
 
+    private void assertAppointmentFallsWithinTeacherWorkingSchedule(Teacher teacher, ScheduledAppointmentCreateRequestDto createRequestDto) {
+        WeeklyWorkingSchedule teacherWeeklySchedule = teacher.getWeeklyWorkingSchedule();
+
+        //start and end date cannot be more than 8 hours apart
+        if(createRequestDto.getEndsOn().isAfter(createRequestDto.getStartsOn().plusHours(8))){
+            throw new BusinessException(ErrorCodes.APPOINTMENT_CANNOT_LAST_MORE_THAN_8_HOURS);
+        }
+
+        DayOfWeek appointmentDay = createRequestDto.getStartsOn().getDayOfWeek();
+        List<DayOfWeek> teacherWorkingDays = getTeacherWorkingDays(teacher);
+        if(!teacherWorkingDays.contains(appointmentDay)){
+            throw new BusinessException(ErrorCodes.TEACHER_DOES_NOT_WORK_ON_APPOINTMENT_DAY);
+        }
+    }
+
+    private List<DayOfWeek> getTeacherWorkingDays(Teacher teacher) {
+        List<DayOfWeek> teacherWorkingDays = new ArrayList<>();
+        if(teacher.getWeeklyWorkingSchedule().isSunday()){
+            teacherWorkingDays.add(DayOfWeek.SUNDAY);
+        }
+        if(teacher.getWeeklyWorkingSchedule().isMonday()){
+            teacherWorkingDays.add(DayOfWeek.MONDAY);
+        }
+        if(teacher.getWeeklyWorkingSchedule().isTuesday()){
+            teacherWorkingDays.add(DayOfWeek.TUESDAY);
+        }
+        if(teacher.getWeeklyWorkingSchedule().isWednesday()){
+            teacherWorkingDays.add(DayOfWeek.WEDNESDAY);
+        }
+        if(teacher.getWeeklyWorkingSchedule().isThursday()){
+            teacherWorkingDays.add(DayOfWeek.THURSDAY);
+        }
+        if(teacher.getWeeklyWorkingSchedule().isFriday()){
+            teacherWorkingDays.add(DayOfWeek.FRIDAY);
+        }
+        if(teacher.getWeeklyWorkingSchedule().isSaturday()){
+            teacherWorkingDays.add(DayOfWeek.SATURDAY);
+        }
+        return teacherWorkingDays;
+    }
+
     private void assertStudentExists(Long studentId) {
         studentRepository.findById(studentId).orElseThrow(() -> new NotFoundException("No Student found for id: " + studentId));
     }
@@ -58,7 +103,7 @@ public class ScheduledAppointmentServiceImpl implements ScheduledAppointmentServ
         return teacherRepository.findById(teacherId).orElseThrow(() -> new NotFoundException("No Teacher found for id: " + teacherId));
     }
 
-    private void assertTeacherScheduleIsAvailable(Teacher teacher, ScheduledAppointmentCreateRequestDto scheduledAppointment) {
+    private void assertTeacherTimeSlotIsAvailable(Teacher teacher, ScheduledAppointmentCreateRequestDto scheduledAppointment) {
         List<ScheduledAppointment> existingAppointments = teacher.getScheduledAppointments();
         //Optional<List<ScheduledAppointment>> existingAppointments = scheduledAppointmentRepository.findByTeacherId(teacher.getId());
 
