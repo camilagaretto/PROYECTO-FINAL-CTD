c4b3060d6a8f7e35c603a9ca3ae6de765114c1b4
julianbordet <bordet.julian@gmail.com>
2023-08-21 18:00:34 -0300

    updated token duration

--- backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/security/service/JwtService.java
+++ backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/security/service/JwtService.java
@@ -38,7 +38,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 2)) //  + 1000 * 60 * 24 -> 24 minutos, *2 48min
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 2 * 24 * 24)) //  + 1000 * 60 * 24 -> 24 minutos
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
