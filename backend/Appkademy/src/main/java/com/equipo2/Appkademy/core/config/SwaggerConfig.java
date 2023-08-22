065e1f3c081b0ed5684816677affa797cd8cc964
julianbordet <bordet.julian@gmail.com>
2023-08-22 15:17:43 -0300

    updated swagger config

--- backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/config/SwaggerConfig.java
+++ backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/config/SwaggerConfig.java
@@ -2,9 +2,6 @@ package com.equipo2.Appkademy.core.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -18,13 +15,7 @@ public class SwaggerConfig {
         return new OpenAPI().addSecurityItem(new SecurityRequirement().
                         addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title("Appkademy REST API")
-                        .description("Appkademy REST API")
-                        .version("1.0").contact(new Contact().name("Julian Bordet")
-                                .email( "ImNotWritingMyEmailHere").url("nope"))
-                        .license(new License().name("TBD")
-                                .url("TBD")));
+                        ("Bearer Authentication", createAPIKeyScheme()));
     }
 
     private SecurityScheme createAPIKeyScheme() {
