c56cbddb4fb2ab7d8733c40c7c7aa76c49c74e21
Luciana Costilla <costillalu@gmail.com>
2023-09-02 22:51:10 +0000

    Update index.jsx

--- frontend/src/Routes/Front/Category/index.jsx
+++ frontend/src/Routes/Front/Category/index.jsx
@@ -14,52 +14,6 @@ const index = () => {
 
   useEffect(() => {
 
-<<<<<<< frontend/src/Routes/Front/Category/index.jsx
-    try {
-      const response = await fetch('http://ec2-107-21-139-55.compute-1.amazonaws.com/v1/categories/1/providers/search', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(postData),
-      });
-      if (response.ok) {
-        const teachers = await response.json();
-        let shuffledItems = []
-        if(teachers.searchResults) {
-          shuffledItems = teachers.searchResults;
-        }
-        setFiltered(shuffledItems);
-      } else {
-        console.log(response)
-      }
-    } catch (error) {
-      console.error('Error de red:', error);
-    }
-  }
-  const getCategories = async () => {
-    const postData = {
-      pageNumber: 1,
-      pageSize: 100,
-    }      
-
-    try {
-        const response = await fetch('http://ec2-107-21-139-55.compute-1.amazonaws.com/v1/categories/1/providers/teaching_subject/search', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(postData),
-        });
-        if (response.ok) {
-          const subjects = await response.json();
-          setSubjects(subjects.searchResults);
-        } else {
-          console.log(response)
-        }
-    } catch (error) {
-        console.error('Error de red:', error);
-=======
     const newObject = {
       pageNumber: 1,
       pageSize: 10,
@@ -70,7 +24,6 @@ const index = () => {
       },
       freeOn: dateTime,
       randomOrder: true
->>>>>>> frontend/src/Routes/Front/Category/index.jsx
     }
 
     Axios.post('http://ec2-107-21-139-55.compute-1.amazonaws.com/v1/categories/1/providers/search', newObject)
