1e3635281ec6789a6e4f61185293fb353c66cf0b
julianbordet <bordet.julian@gmail.com>
2023-09-05 18:01:18 -0300

    updated security config to remove deprecated methods

--- backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/config/SecurityConfig.java
+++ backend/Appkademy/src/main/java/com/equipo2/Appkademy/core/config/SecurityConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,41 +21,26 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity(prePostEnabled = true) //Esto permite que el @PreAuthorize en el controller se tenga en cuenta (sino se lo ignora)
 public class SecurityConfig {
 
-    //TODO: REEMPLAZAR METODOS DEPRECADOS
-
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-
-                .cors().and()
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/v1/auth/**")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET, "/v1/categories/1/providers/{id}")
-                .permitAll()
-                .requestMatchers(HttpMethod.POST, "/v1/categories/1/providers/search")
-                .permitAll()
-                .requestMatchers(HttpMethod.POST, "/v1/categories/1/customers/")
-                .permitAll()
-                .requestMatchers(HttpMethod.POST, "/v1/categories/1/providers/teaching_subject/search")
-                .permitAll()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/webjars/**", "/documentation/**")
-                .permitAll()
-                .requestMatchers("/error")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.GET, "/v1/categories/1/providers/{id}").permitAll()
+                        .requestMatchers("/v1/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/categories/1/providers/search").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/categories/1/customers/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/categories/1/providers/teaching_subject/search").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/webjars/**", "/documentation/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated())
+                        .sessionManagement(sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .authenticationProvider(authenticationProvider).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
