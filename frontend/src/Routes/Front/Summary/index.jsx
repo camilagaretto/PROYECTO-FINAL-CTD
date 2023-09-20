7f1e048905322f2f0561d7d42f41ef35a403f7a7
Alanis Gomez <agomez@aper.com>
2023-09-20 00:15:07 -0300

    Estilos en vista de confirmacion de reserva

--- frontend/src/Routes/Front/Summary/index.jsx
+++ frontend/src/Routes/Front/Summary/index.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import moment from 'moment';
 import Appointments from '../../../Components/Appointments';
+import './styles.scss'
 
 const Summary = () => {
     const navigate = useNavigate();
@@ -35,6 +36,7 @@ const Summary = () => {
             `http://localhost:8080/v1/categories/1/providers/${params.teacherId}`
         );
         const data = await response.json();
+        console.log(data)
         setTeacherData(data);
         } catch (error) {
         console.error('Error al obtener los datos:', error);
@@ -75,60 +77,67 @@ const Summary = () => {
 
     return (
         <main>
-            <Container className='detail-container'>
-                <section className='detail-left'>
-                    <h2>Revisá si todo está bien</h2>
-                    <Appointments events={events}/>
-                    <h4>Tus datos</h4>
-                    <form className="row g-3">
-                        <div className="col-md-6">
-                            <label htmlFor="firstName" className="form-label">Nombre</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="firstName"
-                                name="firstName"
-                                value={userObj.firstName}
-                                disabled
-                            />
-                        </div>
-                        <div className="col-md-6">
-                            <label htmlFor="lastName" className="form-label">Apellido</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="lastName"
-                                name="lastName"
-                                value={userObj.lastName}
-                                disabled
-                            />
-                        </div>
-                        <div className="col-md-12">
-                            <label htmlFor="email" className="form-label">Email</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="email"
-                                name="email"
-                                value={userObj.email}
-                                disabled
-                            />
-                        </div>
-                    </form>
-                </section>
-                <section className='detail-right'>
-                    <h4>Detalles</h4>
-                    <div>
-                        {teacherData.firstName} {teacherData.lastName}
-                        <p>Profesor de </p>
+            <Container>
+                <div className='row'>
+                <h2>Revisá si todo está bien</h2>
+                <section className='col-12 col-md-7'>
+                    <div className='calendar-container'>
+                        <Appointments events={events}/>
+                        <h4 className='mt-3'>Tus datos</h4>
+                        <form className="row g-3">
+                            <div className="col-md-6">
+                                <label htmlFor="firstName" className="form-label">Nombre</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="firstName"
+                                    name="firstName"
+                                    value={userObj.firstName}
+                                    disabled
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <label htmlFor="lastName" className="form-label">Apellido</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="lastName"
+                                    name="lastName"
+                                    value={userObj.lastName}
+                                    disabled
+                                />
+                            </div>
+                            <div className="col-md-12">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="email"
+                                    name="email"
+                                    value={userObj.email}
+                                    disabled
+                                />
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                        Hora de clase
-                        <p>Inicio: {formatearFechaHora(params.startDate)} </p>
-                        <p>Fin: {formatearFechaHora(params.endDate)}</p>
-                    </div>
-                    <button className='btn btn-primary' onClick={handleSubmit}>Confirmar reserva</button>
                 </section>
+                <section className='col-12 col-md-5'>
+                    <div className='summary-container'>
+                        <h4>Profesor {teacherData.firstName} {teacherData.lastName}</h4>
+                        <img src={teacherData.profilePictureUrl} alt="Appkademy teacher image" />
+                        <div>
+                            <h5>Descripción</h5>
+                            <p>{teacherData.shortDescription}</p>
+                        </div>
+                        <div>
+                            <h5>Hora de clase</h5>
+                            <p><b>Inicio:</b> {formatearFechaHora(params.startDate)} </p>
+                            <p><b>Fin:</b> {formatearFechaHora(params.endDate)}</p>
+                        </div>
+                    </div>
+                    <button className='btn btn-primary btn-lg w-100 mt-4' onClick={handleSubmit}>Confirmar reserva</button>
+                </section>
+                </div>
             </Container>
         </main>
     )
